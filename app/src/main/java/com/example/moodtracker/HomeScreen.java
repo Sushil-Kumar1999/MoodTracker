37c0499d9d92c160d2918af70ebfc41084b46a66
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-10 11:28:54 +0100

    activities

---
+++ app/src/main/java/com/example/moodtracker/HomeScreen.java
@@ -0,0 +1,21 @@
+package com.example.moodtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
